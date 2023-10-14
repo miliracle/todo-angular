417159eaced0fd04aa6f80059c74ea00db3b6326
miliracle <nguyennhathan126@gmail.com>
2023-10-14 23:29:03 +0700

    add env

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GetHourlyForecastParams, ResultGetHourlyForecast } from './weather';
